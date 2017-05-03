43f2e76d105bcdd396111d401db0895ca743395c
Asia Kane and Aubrey Masten <asia@>
2017-05-03 13:27:06 -0700

    add method to retrieve train id

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -7,4 +7,11 @@ describe Train do
       expect(test_train.color).to eq 'blue'
     end
   end
+
+  describe '#id' do
+    it 'returns the unique id of a specific train' do
+      test_train = Train.new({:color => 'blue', :id => nil})
+      expect(test_train.id).to eq nil
+    end
+  end
 end
