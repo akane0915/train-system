85c53e159b931318b44878d0d1025da9a906cead
Asia Kane and Aubrey Masten <aubrey@>
2017-05-04 11:22:34 -0700

    add front-end code and tests to display static home, train and city pages

--- app.rb
+++ app.rb
@@ -2,10 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './lib/train'
 require './lib/city'
-require 'pry'
-
 also_reload('lib/**/*.rb')
+require 'pry'
+require 'pg'
 
-get('/') do
-  erb(:index)
+DB = PG.connect({:dbname => 'train_system'})
+
+get '/' do
+  @trains = Train.all
+  @cities = City.all
+  erb :index
+end
+
+get '/trains' do
+  @trains = Train.all
+  erb :trains
+end
+
+get '/cities' do
+  @cities = City.all
+  erb :cities
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,3 +3,26 @@ require "./app"
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe 'the homepage display path', {:type => :feature} do
+  it 'shows a welcome message' do
+    visit '/'
+    expect(page).to have_content "Welcome to the Oregon Train Website!"
+  end
+end
+
+describe 'the train creation path', {:type => :feature} do
+  it 'takes the user to a page where they can create a new train' do
+    visit '/'
+    click_link 'Add a new train'
+    expect(page).to have_content 'Welcome to the Train page'
+  end
+end
+
+describe 'the city creation path', {:type => :feature} do
+  it 'takes the user to a page where they can create a new city' do
+    visit '/'
+    click_link 'Add a new city'
+    expect(page).to have_content 'Welcome to the City page'
+  end
+end
